f20baaa4a0d7b57870be59512b8fc49bf52636e9
Utopian <edwi.castruita117@gmail.com>
2024-11-04 17:35:31 -0600

    se arreglo pagina dinamica con next 15

--- src/app/(protected)/[mediaId]/page.tsx
+++ src/app/(protected)/[mediaId]/page.tsx
@@ -1,43 +1,9 @@
-"use client";
 import BannerComponent from "@/components/detailsComponents/BannerComponent";
-import { movieById } from "@/lib/api";
-import { useEffect, useState } from "react";
 
-interface PropsMovie {
-  title: string;
-  poster_path: string;
-  vote_average: number;
-  release_date: string;
-  overview: string;
-  genres: string[];
-}
-const Details = ({ params }: { params: { mediaId: string } }) => {
-  const [movieData, setMovieData] = useState<PropsMovie | undefined>();
+const Details = async ({ params }: { params: { mediaId: string } }) => {
+  const { mediaId } = await params;
 
-  useEffect(() => {
-    const fetchMovieData = async () => {
-      try {
-        const data = await movieById(params.mediaId);
-        setMovieData(data);
-      } catch (error) {
-        console.error("Error al obtener los datos de la película:", error);
-      }
-    };
-
-    fetchMovieData();
-  }, []);
-  return movieData ? (
-    <BannerComponent
-      poster_path={movieData.poster_path}
-      title={movieData.title}
-      vote_average={movieData.vote_average}
-      release_date={movieData.release_date}
-      overview={movieData.overview}
-      genres={movieData.genres}
-    />
-  ) : (
-    <h1 className="text-white">Cargando...</h1>
-  );
+  return <BannerComponent mediaId={mediaId} />;
 };
 
 export default Details;
